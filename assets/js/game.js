cf97e6b5ba46b30df013e72da94bd7c284205ed2
Your Name <jasjohn3@uat.edu>
2021-06-21 10:56:00 -0700

    Completed MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,7 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 var enemyAttack = 12;
+var enemyHealth = 50;
 
 var enemyNames = ["Roborto","Amy Android", "Robo Trumble"];
 
@@ -65,21 +66,42 @@ var fight = function(enemyName) {
     }
   }
 };
-
-for (let i = 0; i< enemyNames.length; i++) {
+var startGame = function(){
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+ for(var i = 0; i < enemyNames.length; i++){
   if(playerHealth > 0){
     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
     var pickedEnemyName = enemyNames[i];
-    var enemyHealth = 50;
+    enemyHealth = 50;
     fight(pickedEnemyName); 
   }  else {
     window.alert("You have lost your robot in battle! Game Over!");
     break;
   }  
-}
+ }
+ endGame();
+};
+var endGame = function(){
+  window.alert("The game has now ended.  Let's se how you did!");
+  if(playerHealth > 0){
+    window.alert("Great Job! You have survived the game! You now have a score of "+playerMoney+".");
+  }else{
+    window.alert("You've lost your robot in battle.");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if(playAgainConfirm){
+    startGame();
+  }else{
+    window.alert("Thank you for playing robot gladiators! Come back soon!");
+  }
+};
 
 // run fight function to start game
-// fight();
+debugger;
+startGame();
 
 //Game States
 //"WIN" - Player robot has defeated all enemy robots
